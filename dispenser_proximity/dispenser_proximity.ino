d4198c3de61d06b2c41298a027d9c594e1587069
polar1510 <tdavsys@gmail.com>
2020-09-01 20:54:27 +1000

    Started developing program to run on BLE on dispenser - forwards any data received over BLE to serial. Will only connect to wearables with the specific UUID

---
+++ dispenser_proximity/dispenser_proximity.ino
@@ -0,0 +1,153 @@
+#include <bluefruit.h>
+
+const uint8_t CUSTOM_UUID[] =
+{
+    0xA0, 0xDB, 0xD3, 0x6A, 0x00, 0xA6, 0xF7, 0x8C,
+    0xE7, 0x11, 0x8F, 0x71, 0x1A, 0xFF, 0x67, 0xDF
+};
+
+char last_connected[4]; // used to keep track of name of last connected wearable - so that wearable doesn't keep spamming the list
+
+BLEUuid uuid = BLEUuid(CUSTOM_UUID);
+
+BLEClientUart clientUart; // bleuart client (central)
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  while ( !Serial ) delay(10);   // for nrf52840 with native usb
+
+  Serial.println("Bluefruit52 Central BLEUART Example");
+  Serial.println("-----------------------------------\n");
+  
+  // Initialize Bluefruit with maximum connections as Peripheral = 0, Central = 1
+  // SRAM usage required by SoftDevice will increase dramatically with number of connections
+  Bluefruit.begin(0, 1);
+  
+  Bluefruit.setName("S001");
+
+  // Init BLE Central Uart Serivce
+  clientUart.begin();
+  clientUart.setRxCallback(bleuart_rx_callback);
+
+  // Increase Blink rate to different from PrPh advertising mode
+  Bluefruit.setConnLedInterval(250);
+
+  // Callbacks for Central
+  Bluefruit.Central.setConnectCallback(connect_callback);
+  Bluefruit.Central.setDisconnectCallback(disconnect_callback);
+
+  /* Start Central Scanning
+   * - Enable auto scan if disconnected
+   * - Interval = 100 ms, window = 80 ms
+   * - Don't use active scan
+   * - Start(timeout) with timeout = 0 will scan forever (until connected)
+   */
+  Bluefruit.Scanner.setRxCallback(scan_callback);
+  Bluefruit.Scanner.restartOnDisconnect(true);
+  Bluefruit.Scanner.filterUuid(uuid);           // Only invoke callback if the target UUID was found
+  Bluefruit.Scanner.setInterval(160, 80); // in unit of 0.625 ms
+  Bluefruit.Scanner.useActiveScan(false);
+  Bluefruit.Scanner.start(0);                   // // 0 = Don't stop scanning after n seconds
+
+}
+
+void scan_callback(ble_gap_evt_adv_report_t* report) // needs to be modified
+{
+  // Check if advertising contain BleUart service
+  if ( Bluefruit.Scanner.checkReportForService(report, clientUart) )
+  {
+    Serial.print("BLE UART service detected. Connecting ... ");
+
+    // Connect to device with bleuart service in advertising
+    Bluefruit.Central.connect(report);
+  }else
+  {      
+    // For Softdevice v6: after received a report, scanner will be paused
+    // We need to call Scanner resume() to continue scanning
+    Bluefruit.Scanner.resume();
+  }
+}
+
+/**
+ * Callback invoked when an connection is established
+ * @param conn_handle
+ */
+void connect_callback(uint16_t conn_handle) // needs to be modified
+{
+  Serial.println("Connected");
+  Serial.print("Discovering BLE Uart Service ... ");
+  if ( clientUart.discover(conn_handle) )
+  {
+    Serial.println("Found it");
+
+    Serial.println("Enable TXD's notify");
+    clientUart.enableTXD();
+
+    Serial.println("Ready to receive from peripheral");
+  }else
+  {
+    Serial.println("Found NONE");
+    
+    // disconnect since we couldn't find bleuart service
+    Bluefruit.disconnect(conn_handle);
+  }  
+}
+
+/**
+ * Callback invoked when a connection is dropped
+ * @param conn_handle
+ * @param reason is a BLE_HCI_STATUS_CODE which can be found in ble_hci.h
+ */
+void disconnect_callback(uint16_t conn_handle, uint8_t reason)
+{
+  (void) conn_handle;
+  (void) reason;
+  
+  Serial.print("Disconnected, reason = 0x"); Serial.println(reason, HEX);
+}
+
+/**
+ * Callback invoked when uart received data
+ * @param uart_svc Reference object to the service where the data 
+ * arrived. In this example it is clientUart
+ */
+void bleuart_rx_callback(BLEClientUart& uart_svc)
+{
+  Serial.print("[RX]: ");
+  
+  while ( uart_svc.available() )
+  {
+    Serial.print( (char) uart_svc.read() );
+  }
+
+  Serial.println();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+
+
+    // Forward from Serial to BLEUART
+  if (Serial.available())
+  {
+    // Delay to get enough input data since we have a
+    // limited amount of space in the transmit buffer
+    delay(20);
+ 
+    uint8_t buf[64];
+    int count = Serial.readBytes(buf, sizeof(buf));
+    clientUart.write( buf, count );
+  }
+ 
+  // Forward from BLEUART to Serial
+  if ( clientUart.available() )
+  {
+    uint8_t ch;
+    ch = (uint8_t) clientUart.read();
+    Serial.write(ch);
+  }
+
+}

cd61aaf677a9c9e27b384b37e50a81beb045176c
tjdav10 <tjdav10@student.monash.edu>
2020-09-24 20:04:45 +1000

    Update string_parse.py

--- RasPi/string_parse.py
+++ RasPi/string_parse.py
@@ -12,7 +12,6 @@ while 1:
     if(ser.in_waiting >0):
         line = ser.readline()
         decode = line.decode('utf-8') # decoding into string
-<<<<<<< HEAD
         if decode[4]==" ":
             x = decode.split() #splitting using space as delimitor
             print(x)
@@ -32,7 +31,6 @@ while 1:
                 c.execute("INSERT INTO interaction_log VALUES (?,?,?,?)", for_upload)
                 conn.commit()
                 received_time()
-=======
         x = decode.split() #splitting using space as delimitor
         print(x)
         if(x(0)=="B"):
@@ -42,4 +40,3 @@ while 1:
             c = conn.cursor()
             c.execute("INSERT INTO test_table VALUES (?,?)",x) ## can be changed to specify the fields
             conn.commit()
->>>>>>> 6e810468ec67ed80f873acfcc7bc0a067c9eb058

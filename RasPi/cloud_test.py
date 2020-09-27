ac774290051c66b5a264df3e32942611f93eaa11
Tim <tjdav10@student.monash.edu>
2020-09-27 15:31:03 +1000

    updated cloud_test to work with new cloud db

--- RasPi/cloud_test.py
+++ RasPi/cloud_test.py
@@ -1,30 +1,34 @@
 import serial
-import pymysql
+import pymysql.cursors
 import string
 
 # ser = serial.Serial('/dev/ttyACM0', 115200) # opening serial port, ACM0 is top left USB port
-conn = pymysql.connect(host='68.183.123.37',
+conn = pymysql.connect(host='128.199.68.151',
                        user='tim',
                        password='monashtim123',
-                       db='db',
-                       port=8081,
+                       db='hand_hygiene',
+                       port=3306,
                        cursorclass=pymysql.cursors.DictCursor)
 print("connected")
 while 1:
     print('enter battery log')
     x = input()
     x = x.split()
-    print(x)
+#     print(x)
     with conn.cursor() as cursor:
         id = [x[0]]
         level = x[1]
-        cursor.execute('SELECT COUNT(1) FROM battery WHERE id = ?',id) # checking if record exists
-        y = cursor.fetchone()
-        if(y[0]==1):
+        cursor.execute('SELECT * FROM battery WHERE id = %s',id) # checking if record exists
+        y = cursor.fetchall()
+        print(y)
+        y = cursor.rowcount
+        print(y)
+        if(y!=0):
+            print("updating")
             data = [level, id[0]]
-            print(data)
-            cursor.execute('UPDATE battery SET level = ? WHERE id = ?', data)
+            cursor.execute('UPDATE battery SET level = %s WHERE id = %s', data)
             conn.commit()
         else:
-             cursor.execute('INSERT INTO battery VALUES (?,?)',x)
-             conn.commit()
+            print("creating")
+            cursor.execute('INSERT INTO battery VALUES (%s,%s)',x)
+            conn.commit()

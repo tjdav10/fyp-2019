50a2ae4823905e8c3423c15ca3800762a33e7b59
Tim <tjdav10@student.monash.edu>
2020-09-27 15:49:12 +1000

    added decision making in cloud_test.py

--- RasPi/cloud_test.py
+++ RasPi/cloud_test.py
@@ -2,7 +2,7 @@ import serial
 import pymysql.cursors
 import string
 
-# ser = serial.Serial('/dev/ttyACM0', 115200) # opening serial port, ACM0 is top left USB port
+ser = serial.Serial('/dev/ttyACM0', 115200) # opening serial port, ACM0 is top left USB port
 conn = pymysql.connect(host='128.199.68.151',
                        user='tim',
                        password='monashtim123',
@@ -11,24 +11,36 @@ conn = pymysql.connect(host='128.199.68.151',
                        cursorclass=pymysql.cursors.DictCursor)
 print("connected")
 while 1:
-    print('enter battery log')
-    x = input()
-    x = x.split()
-#     print(x)
-    with conn.cursor() as cursor:
-        id = [x[0]]
-        level = x[1]
-        cursor.execute('SELECT * FROM battery WHERE id = %s',id) # checking if record exists
-        y = cursor.fetchall()
-        print(y)
-        y = cursor.rowcount
-        print(y)
-        if(y!=0):
-            print("updating")
-            data = [level, id[0]]
-            cursor.execute('UPDATE battery SET level = %s WHERE id = %s', data)
-            conn.commit()
-        else:
-            print("creating")
-            cursor.execute('INSERT INTO battery VALUES (%s,%s)',x)
-            conn.commit()
+    if(ser.in_waiting >0):
+        line = ser.readline()
+        decode = line.decode('utf-8') # decoding into string
+        with conn.cursor() as cursor:
+            x = decode.split() #splitting using space as delimitor
+            print(x)
+            if x[0]=="B": # battery reading
+                v_meas = float(x[2])
+                v_max = float(x[3])
+                v_min = float(x[4])
+                level = int((v_meas-v_min)/(v_max-v_min)*100) # computing battery as %
+                id = [x[1]]
+                cursor.execute('SELECT * FROM battery WHERE id = %s',id) # checking if record exists
+                y = cursor.fetchall()
+                print(y)
+                y = cursor.rowcount
+                print(y)
+                if(y!=0):
+                    print("updating")
+                    data = [level, id[0]]
+                    cursor.execute('UPDATE battery SET level = %s WHERE id = %s', data)
+                    conn.commit()
+                else:
+                    print("creating")
+                    data = [id[0], level]
+                    cursor.execute('INSERT INTO battery VALUES (%s,%s)',data)
+                    conn.commit()
+            elif (len(decode)>1):# if its interaction log - todo
+                
+                
+                
+                
+            elif (len(decode)==1):# if its hand hygiene log - do original code stuff
